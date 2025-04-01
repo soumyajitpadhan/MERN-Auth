55f07b9398689a36c16089ffb1c03f90eefd14b9
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2025-04-01 21:17:17 +0530

    MERN-Auth

--- Server/config/mongodb.js
+++ Server/config/mongodb.js
@@ -11,7 +11,6 @@ const connectToDB = async () => {
     catch (error) {
         console.log("Failed to connect DB");
         console.error(error);
-        process.exit(1); // Exit the process if DB connection fails
     }
 }
 
--- Server/index.js
+++ Server/index.js
@@ -31,7 +31,6 @@ const startServer = async () => {
     }
     catch (error) {
         console.error("Database connection failed:", error);
-        process.exit(1); // Exit the process if DB connection fails
     }
 }
 
